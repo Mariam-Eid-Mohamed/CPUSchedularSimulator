ce2509795d72e105370b8ba13242ea0f7c63f8f7
Medhat Ramadan <136108426+MedhatRamdan@users.noreply.github.com>
2024-12-02 20:32:57 +0200

    Update FCAIScheduler.java

--- src/main/java/com/cpuscheduler/FCAIScheduler.java
+++ src/main/java/com/cpuscheduler/FCAIScheduler.java
@@ -3,6 +3,40 @@ import java.util.List;
 
 public class FCAIScheduler {
 
+    
+    public double[] calculateV1V2(List<FCAIProcess> processes) {
+        // Find the maximum arrival time and maximum burst time among all processes
+        int maxArrivalTime = 0;
+        int maxRemainingBurstTime = 0;
+        for (FCAIProcess process: processes) {
+            if (process.getArrivalTime() > maxArrivalTime)
+                maxArrivalTime = process.getArrivalTime();
+            if (process.getRemainingBurstTime() > maxRemainingBurstTime)
+                maxRemainingBurstTime = process.getRemainingBurstTime();
+        }
+        // Calculate V1 and V2 according to the formula
+        double V1 = (double) maxArrivalTime / 10;
+        double V2 = (double) maxRemainingBurstTime / 10;
+
+        // Return both values as an array
+        return new double[]{V1, V2};
+    }
+
+    public static void updateFCAIFactor(double V1, double V2, List<FCAIProcess> processes) {
+        // set V1 & V2 and then updating the FCAI factor
+        for (FCAIProcess process: processes) {
+            process.setV1(V1);
+            process.setV2(V2);
+            process.updateFCAIFactor();
+        }
+    }
+
+    public static void displayFCAIFactor(List<FCAIProcess> processes) {
+        for (FCAIProcess process : processes) {
+            System.out.println("Process " + process.getName() + ": " + process.getFCAIFactor());
+        }
+        System.out.println("______________________________________________________________________________________");
+    }
     // schedule processes using the FCAI scheduling algorithm
     public void scheduleProcesses(List<Process> processList);
 
