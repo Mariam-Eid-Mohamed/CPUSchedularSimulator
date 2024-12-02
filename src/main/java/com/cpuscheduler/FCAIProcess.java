520b9e6b0835b5a1d5229244c5bc4f9c74323005
Medhat Ramadan <136108426+MedhatRamdan@users.noreply.github.com>
2024-12-02 17:50:02 +0200

    Create FCAIProcess.java

---
+++ src/main/java/com/cpuscheduler/FCAIProcess.java
@@ -0,0 +1,28 @@
+package com.cpuscheduler;
+
+public class FCAIProcess extends Process {
+    private int quantum;      
+    private double FCAIFactor;
+
+    public FCAIProcess(String name, String color, int arrivalTime, int burstTime, int priority, int quantum) {
+        super(name, color, arrivalTime, burstTime, priority); // Call to superclass constructor
+        this.quantum = quantum;
+        this.FCAIFactor = 0.0; // Initialize FCAIFactor to a default value
+    }
+
+    public int getQuantum() {return quantum;}
+    public void setQuantum(int quantum) {this.quantum = quantum;}
+    public double getFCAIFactor() {return FCAIFactor;}
+
+    public void updateQuantum(int executedTime) {
+        if (remainingBurst > 0) {
+            if (executedTime < quantum)
+                quantum += quantum - executedTime;
+            else
+                quantum += 2;
+        }
+    }
+    public void updateFCAIFactor(double V1, double V2) {
+        
+    }
+}
