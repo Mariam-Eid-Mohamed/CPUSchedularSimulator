a7e3eaa11fad9f57477e5ec3a81b113c855e6a85
Medhat Ramadan <136108426+MedhatRamdan@users.noreply.github.com>
2024-12-02 18:18:30 +0200

    Update FCAIScheduler.java

--- src/main/java/com/cpuscheduler/FCAIScheduler.java
+++ src/main/java/com/cpuscheduler/FCAIScheduler.java
@@ -3,15 +3,6 @@ import java.util.List;
 
 public class FCAIScheduler {
 
-    // calculate V1 and V2 from the list of processes
-    public void calculateFactors(List<Process> processList);
-
-    // calculate FCAI Factor for a given process
-    public int calculateFCAIFactor(Process process, double V1, double V2);
-
-    // update quantum dynamically after a process is preempted or completes
-    public void updateQuantum(Process process, boolean isPreempted, int unusedQuantum);
-
     // schedule processes using the FCAI scheduling algorithm
     public void scheduleProcesses(List<Process> processList);
 
