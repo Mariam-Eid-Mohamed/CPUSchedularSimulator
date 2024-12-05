3cea86a6013071dacb069bfd88ce05daf4244233
Medhat Ramadan <136108426+MedhatRamdan@users.noreply.github.com>
2024-12-05 20:10:31 +0200

    Update FCAIProcess.java

--- src/main/java/com/cpuscheduler/FCAIProcess.java
+++ src/main/java/com/cpuscheduler/FCAIProcess.java
@@ -2,29 +2,33 @@ package com.cpuscheduler;
 
 public class FCAIProcess extends Process {
     private int quantum;
-    private double FCAIFactor;
-
-    public FCAIProcess(String name, String color, int arrivalTime, int burstTime, int priority, int quantum) {
-        super(name, color, arrivalTime, burstTime, priority); // Call to superclass constructor
+    private int FCAIFactor;
+    public int prvFCAIFactor;
+    public int prvQuantum;
+    public boolean isCompleted = false;
+    public FCAIProcess(String name, String color,int burstTime, int arrivalTime, int priority, int quantum) {
+        super(name, color,burstTime, arrivalTime, priority); // Call to superclass constructor
         this.quantum = quantum;
-        this.FCAIFactor = 0.0; // Initialize FCAIFactor to a default value
+        this.FCAIFactor = 0; // Initialize FCAIFactor to a default value
     }
 
     public int getQuantum() {return quantum;}
     public void setQuantum(int quantum) {this.quantum = quantum;}
-    public double getFCAIFactor() {return FCAIFactor;}
-    public void setFCAIFactor(double FCAIFactor) {this.FCAIFactor = FCAIFactor;}
-
+    public int getFCAIFactor() {return FCAIFactor;}
+    public void setFCAIFactor(int FCAIFactor) {this.FCAIFactor = FCAIFactor;}
     public void updateQuantum(int executedTime) {
-        if (getRemainingBurstTime() > 0) {
-            if (executedTime < quantum)
-                quantum += quantum - executedTime;
-            else
-                quantum += 2;
-        }
+        if (executedTime == quantum && getRemainingBurstTime() > 0)
+            quantum += 2;
+        else
+            quantum += (quantum - executedTime);
     }
     public void updateFCAIFactor(double V1, double V2) {
-        FCAIFactor = Math.ceil((10 - getPriority()) + (getArrivalTime() / V1) + (getRemainingBurstTime() / V2));
+        FCAIFactor = (int) Math.ceil((10 - getPriority()) + ((double) getArrivalTime() / V1) + ((double) getRemainingBurstTime() / V2));
+    }
+
+    public void decrementRBT(int val) {
+        setRemainingBurstTime(getRemainingBurstTime() - val);
+        isCompleted = getRemainingBurstTime() <= 0;
     }
 
 }
